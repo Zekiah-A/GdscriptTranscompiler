0471acd359616806cc619c1171321b1d32ab42c4
starlkytminecraft@gmail.com <starlkytminecraft@gmail.com>
2021-08-25 00:29:58 +0300

    Add a bunch of keywords to `ReservedIdentifiers`.

--- GdscriptTranscompiler/Tokenizing/ReservedIdentifiers.cs
+++ GdscriptTranscompiler/Tokenizing/ReservedIdentifiers.cs
@@ -7,6 +7,26 @@ namespace GdscriptTranscompiler.Tokenizing
         public static readonly IReadOnlyDictionary<string, TokenType> Keywords =
             new Dictionary<string, TokenType>
             {
+                { "if", TokenType.If },
+                { "elif", TokenType.Elif },
+                { "else", TokenType.Else },
+                { "for", TokenType.For },
+                { "while", TokenType.While },
+                { "break", TokenType.Break },
+                { "continue", TokenType.Continue },
+                { "pass", TokenType.Pass },
+                { "return", TokenType.Return },
+                { "extends", TokenType.Extends },
+                { "is", TokenType.Is },
+                { "self", TokenType.Self },
+                { "signal", TokenType.Signal },
+                { "func", TokenType.Func },
+                { "const", TokenType.Const },
+                { "var", TokenType.Var },
+                { "onready", TokenType.OnReady },
+                { "export", TokenType.Export },
+                { "preload", TokenType.Preload },
+                { "yield", TokenType.Yield },
             };
     }
 }
