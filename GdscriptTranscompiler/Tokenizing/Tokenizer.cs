e989e4cf5b04706afe8e1cb4adf1df897bbac44c
starlkytminecraft@gmail.com <starlkytminecraft@gmail.com>
2021-08-25 00:16:23 +0300

    Create the `Tokenizer` class and give it template functionality.

---
+++ GdscriptTranscompiler/Tokenizing/Tokenizer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GdscriptTranscompiler.Tokenizing
+{
+    public class Tokenizer
+    {
+        private readonly string source;
+
+        /// <param name="source">The old GDScript source-code.</param>
+        public Tokenizer(string source)
+        {
+            this.source = source;
+        }
+
+        /// <summary>
+        /// Converts old GDScript to a series of tokens.
+        /// </summary>
+        /// <returns>A read-only span of <see cref="Token"/>.</returns>
+        public ReadOnlySpan<Token> Run()
+        {
+            var tokens = new List<Token>();
+
+            return tokens.ToArray();
+        }
+    }
+}
